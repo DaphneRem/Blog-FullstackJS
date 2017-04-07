da5ba5b1d3f059192c5521f359345413fafec855
daphne remali <remalidaphne@gmail.com>
2017-04-07 18:50:59 +0200

    add index components

---
+++ client/src/components/index.js
@@ -0,0 +1,33 @@
+import About from './About.jsx';
+import Contact from './Contact.jsx';
+import Form from './Form.jsx';
+import Article from './Article.jsx';
+import ArticlesListManager from './ArticlesListManager.jsx';
+import Archive from './Archive.jsx';
+import Base from './Base.jsx';
+import Dashboard from './Dashboard.jsx';
+import HomePage from './HomePage.jsx';
+import LoginForm from './LoginForm.jsx';
+import SignUpForm from './SignUpForm.jsx';
+
+
+
+
+
+
+
+
+// We export all the components at once
+export {
+  About,
+  Contact,
+  Form,
+  Article,
+  ArticlesListManager,
+  Archive,
+  Base,
+  Dashboard,
+  HomePage,
+  LoginForm,
+  SignUpForm
+};

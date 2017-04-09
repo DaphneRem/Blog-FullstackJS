a807138b2780e75562b2965c185a828f0c3934a8
daphne remali <remalidaphne@gmail.com>
2017-04-09 18:29:31 +0200

    add index containers

---
+++ client/src/containers/index.js
@@ -0,0 +1,18 @@
+import AddArticleContainer from './AddArticleContainer.jsx';
+import ArticlesContainer from './ArticlesContainer.jsx';
+import LoginPage from './LoginPage.jsx';
+import SignUpPage from './SignUpPage.jsx';
+import DashboardPage from './DashboardPage.jsx';
+import ArticlesContainerClient from './ArticlesContainerClient';
+
+
+
+// We export all the containers at once
+export {
+  AddArticleContainer,
+  ArticlesContainer,
+  LoginPage,
+  SignUpPage,
+  DashboardPage,
+  ArticlesContainerClient
+};
